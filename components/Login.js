dec0f0641b10e1616f903e5afd5bd82b13781d72
Harsh <57228178+harsh3294@users.noreply.github.com>
2021-12-23 17:21:32 +0530

    updated logo

--- components/Login.js
+++ components/Login.js
@@ -1,19 +1,20 @@
 import Image from "next/image";
 import { useMoralis } from "react-moralis";
+import Meta from "../asset/Meta.png";
 function Login() {
   const { authenticate } = useMoralis();
   return (
     <div className="bg-black relative text-white">
       <div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4">
         <Image
-          src="https://links.papareact.com/3pi"
-          width={200}
+          src={Meta}
+          width={270}
           height={200}
           className="rounded-full"
           objectFit="cover"
         />
         <button
-          className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse text-black"
+          className="bg-yellow-500 rounded-xl p-6 font-bold animate-pulse text-black"
           onClick={authenticate}
         >
           Login to METAVERSE
