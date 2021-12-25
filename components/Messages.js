94724429596e195338b159c2853244108b332d45
Harsh <57228178+harsh3294@users.noreply.github.com>
2021-12-25 18:33:29 +0530

    displaying message

--- components/Messages.js
+++ components/Messages.js
@@ -2,9 +2,25 @@ import { useRef } from "react";
 import { ByMoralis, useMoralis, useMoralisQuery } from "react-moralis";
 import Message from "./Message";
 import SendMessage from "./SendMessage";
+
+const MINS_DURATION = 60;
 function Messages() {
   const { user } = useMoralis();
   const endOfMessagesRef = useRef(null);
+  const { data, loading, error } = useMoralisQuery(
+    "Messages",
+    (query) =>
+      query
+        .ascending("createdAt")
+        .greaterThan(
+          "createdAt",
+          new Date(Date.now() - 1000 * 60 * MINS_DURATION)
+        ),
+    [],
+    {
+      live: true,
+    }
+  );
   return (
     <div className="pb-56">
       <div className="my-5">
@@ -13,8 +29,10 @@ function Messages() {
           variant="dark"
         />
       </div>
-      <div>
-        <Message />
+      <div className="space-y-10 p-4">
+        {data.map((message) => (
+          <Message key={message.id} message={message} />
+        ))}
       </div>
       <div className="flex justify-center">
         <SendMessage endOfMessagesRef={endOfMessagesRef} />
